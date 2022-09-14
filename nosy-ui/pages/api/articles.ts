73d25b3794ab16a576a643f7f8e29d5f2ce787ca
Michael Murphy <michael.w.murphy@outlook.com>
2022-09-14 06:40:26 +0100

    Converts ArticlesApi from a class to exported functions

---
+++ nosy-ui/pages/api/articles.ts
@@ -0,0 +1,127 @@
+import db from "../../config/database";
+import supabase from "../../config/supabase";
+import { Article, ArticleMetadata } from "../../types/Article";
+
+/**
+ * Retrieves all metadata from the database
+ * @returns an array of ArticleMetadata - empty array when error
+ */
+export const getAllMetadata = async (): Promise<ArticleMetadata[]> => {
+  const { data: articles, error } = await supabase
+    .from<ArticleMetadata>(db.tables.articles)
+    .select("id, slug, title, exerpt:content, author, categories(id, name)");
+
+  if (error) {
+    console.error(error);
+    return [];
+  }
+
+  return articles.map((a) => ({ ...a, exerpt: trimExerpt(a.exerpt) }));
+};
+
+/**
+ * Retrieves article metadata based on the unique slug
+ * @param slug the slug of the article metadata to be fetched
+ * @returns ArticleMetadata or `null` in the event of an error
+ */
+export const getMetadataBySlug = async (slug: string): Promise<ArticleMetadata | null> => {
+  const { data, error } = await supabase
+    .from<ArticleMetadata>(db.tables.articles)
+    .select("id, slug, title, exerpt:content, author, categories(id, name)")
+    .eq("slug", slug)
+    .limit(1);
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+
+  if (!data || data.length !== 1) {
+    console.warn(`No article present for slug ${slug}`);
+    return null;
+  }
+
+  return {
+    ...data[0],
+    exerpt: trimExerpt(data[0].exerpt),
+  };
+};
+
+/**
+ * Retrieves all metadata associated with the category
+ * @param category to be filtered on
+ * @returns an array of ArtlcleMetadata - empty in the event of an error
+ */
+export const getMetadatByCategory = async (category: string): Promise<ArticleMetadata[]> => {
+  let { data: articles, error } = await supabase
+    .from<ArticleMetadata>(db.tables.articles)
+    .select("id, slug, title, exerpt:content, author, categories(id, name)");
+
+  // TODO: Figure out how to do this query to filter on the category names
+  // When doing `.eq("category.name", category)` it was returning all but
+  // with only the specified category in the list of categories.
+  // Current fix is to filter in JS.
+
+  if (error || articles === null) {
+    console.error(error);
+    return [];
+  }
+
+  articles = articles.filter((a) => a.categories.map((c) => c.name).includes(category));
+  return articles.map((a) => ({ ...a, exerpt: trimExerpt(a.exerpt) }));
+};
+
+/**
+ * Retrieves the content body of the article based on the slug
+ * @param slug the slug
+ * @returns string of content or null in the event of an error
+ */
+export const getContentBySlug = async (slug: string): Promise<string | null> => {
+  interface ContenResponse {
+    slug: string;
+    content: string;
+  }
+
+  const { data, error } = await supabase
+    .from<ContenResponse>(db.tables.articles)
+    .select("slug, content")
+    .eq("slug", slug)
+    .limit(1);
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+
+  if (!data || data.length !== 1) {
+    console.warn(`No article present for slug ${slug}`);
+    return null;
+  }
+
+  return data[0].content;
+};
+
+/**
+ * Retrieves an artcile based on the slug
+ * @param slug to be fetched
+ * @returns the Article or null in the event of an error
+ */
+export const getBySlug = async (slug: string): Promise<Article | null> => {
+  const content = await getContentBySlug(slug);
+  const metadata = await getMetadataBySlug(slug);
+
+  if (content === null || metadata === null) {
+    return null;
+  }
+
+  const article: Article = {
+    meta: metadata,
+    content,
+  };
+
+  return article;
+};
+
+const trimExerpt = (content: string): string => {
+  return content.slice(0, 200) + "...";
+};
